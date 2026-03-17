afec6a420c911383da475d37b20338ec5de9a74a
M.N.Gayathri Prasad <242204@slcs.edu.in>
2026-03-17 17:54:21 +0530

    Exercises

--- Internal - II/Scatterplot.R
+++ Internal - II/Scatterplot.R
@@ -8,12 +8,6 @@ data <- read_excel("BDA.xlsx", sheet = 5, range = "A1:E19")
 # check names
 colnames(data)
 
-df_multi <- data.frame(
-  time = data$Years,
-  values = c(data$Sales, data$VC, data$FC),
-  group = rep(c("Sales","VC","FC"), each = 18)
-)
-
 plot(data$Years, data$Sales,
      main = "Scatter plot of sales over the years: 2007-24",
      xlab = "Years",
